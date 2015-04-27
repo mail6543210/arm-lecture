00f88a48ecc6ed22003c9d19028b06e4d443eef4
mail6543210 <mail6543210@yahoo.com.tw>
2015-04-27 19:17:34 +0800

    (1) Make C code prettier

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -7,8 +7,7 @@ int main(int argc, char **argv)
   int number=0;
   int result=0;
 
-  scanf("%d",&number);
+  scanf("%d", &number);
   result = fibonacci(number);   
   printf("The fibonacci sequence at %d is: %d\n", number, result);
 }
-
