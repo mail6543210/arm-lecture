56fceb9c0424c32915d2974fc614f348d7b11a7d
mail6543210 <mail6543210@yahoo.com.tw>
2015-04-27 19:37:37 +0800

    (1) Add recursive version fib.s

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -14,19 +14,30 @@ fibonacci:
 	push {r3, r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
+	MOVS r4, r0
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+	SUBS r0, #0
+	BLS .L3
 
 	@ Compare R4 wtih 1
 	@ If R4 == 1 goto .L4 (which returns 1)
+	CMP r0, #1
+	BEQ .L4
 
 	@ R0 = R4 - 1
+	SUB r0, r4, #1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
+	BL fibonacci
 
 	@ R5 = R0
+	MOV r5, r0
 	@ R0 = R4 - 2
+	SUB r0, r4, #2
 	@ Recursive call to fibonacci with R4 - 2 as parameter
+	BL fibonacci
 
 	@ R0 = R5 + R0 (update flags)
+	ADDS r0, r5, r0
 
 	pop {r3, r4, r5, pc}		@EPILOG
 
